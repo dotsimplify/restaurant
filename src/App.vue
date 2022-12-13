c6ecfc14cf3393f7be02dc0a43fa1ac67465a532
dotsimplify <subhash227@yahoo.com>
2022-12-14 01:47:42 +1000

    push to deploy

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <Menu />
+  <Footer />
+</template>
+<script>
+import Header from "./components/layout/Header.vue";
+import Menu from "./components/Menu.vue";
+import Footer from "./components/layout/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Menu,
+    Footer,
+  },
+};
+</script>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 footer-col">
+          <div class="footer_contact">
+            <h4>Contact Us</h4>
+            <div class="contact_link_box">
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span> Call +79680693409 </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> subhash227@yahoo.com </span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+          <div class="footer_detail">
+            <a href="" class="footer-logo"> Feane </a>
+            <p>
+              Necessary, making this the first true generator on the Internet.
+              It uses a dictionary of over 200 Latin words, combined with
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4 footer-col">
+          <h4>Opening Hours</h4>
+          <p>Everyday</p>
+          <p>10.00 Am -10.00 Pm</p>
+        </div>
+      </div>
+      <div class="footer-info">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a target="_blank" href="https://github.com/dotsimplify"
+            >Dot Simplify</a
+          >
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
